6801a3250d960af49b4e9dab60ae58532feea4a6
Xinkai Zhang <xinkaizh@gmail.com>
2025-10-18 14:41:41 -0700

    Reorganize files

--- src/accuracy_check/run_testcases.py
+++ src/accuracy_check/run_testcases.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import importlib.util
-from testcase_utils import*
+from accuracy_check.testcase_utils import *
 
-test_cases_dir = os.path.abspath(os.path.join(os.getcwd(), "test_cases"))
+
+test_cases_dir = os.path.abspath(os.path.join(os.getcwd(), "dataset/test_cases"))
 if test_cases_dir not in sys.path:
     sys.path.insert(0, test_cases_dir)
 
@@ -25,24 +26,3 @@ def test_accuracy(task_id: str, soln_func):
     passed, total, accuracy = test_cases.calculate_accuracy(soln_func)
     print(f"Accuracy: {accuracy:.1f}% ({passed}/{total})")
     print(f'Passed: {passed}/{total} ({accuracy:.1f}%)')
-
-
-if __name__ == "__main__":
-    code_str = """
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {} 
-
-        for i, num in enumerate(nums):
-            
-            complement = target - num
-
-            if complement in num_map:
-                return [num_map[complement], i]
-            else:
-                num_map[num] = i
-    """
-
-    soln_func = str_to_func(code_str)
-    task_id = "two-sum"
-    test_accuracy(task_id, soln_func)
---
+++ src/main.py
@@ -0,0 +1,36 @@
+import textwrap
+import json
+from judge.llm_judge import LLMJudge
+from accuracy_check.run_testcases import *
+
+
+def main():
+    code_str = textwrap.dedent("""
+    class Solution:
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
+            num_map = {} 
+            for i, num in enumerate(nums):
+                complement = target - num
+                if complement in num_map:
+                    return [num_map[complement], i]
+                else:
+                    num_map[num] = i
+    """)
+
+    soln_func = str_to_func(code_str)
+    task_id = "two-sum"
+    test_accuracy(task_id, soln_func)
+
+    judge = LLMJudge(verbose=False)
+    
+    print("Analyzing time & space complexity...")
+    complexity = judge.analyze_complexity(code_str)
+    print(json.dumps(complexity, indent=4))
+
+    print("Analyzing readability...")
+    readability = judge.analyze_readability(code_str)
+    print(json.dumps(readability, indent=4))
+    
+
+if __name__ == '__main__':
+    main()
